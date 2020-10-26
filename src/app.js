21d55039778ae8dc95867de8e7ad92fc1bd843c6
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-10-26 23:16:21 +0530

    added submisison of post

--- src/app.js
+++ src/app.js
@@ -4,9 +4,37 @@ import { ui } from "./ui";
 // get posts on DOM load
 document.addEventListener("DOMContentLoaded", getPosts);
 
+// listen for add post
+document.querySelector(".post-submit").addEventListener("click", submitPost);
+
+// listen for delete
+document.querySelector("#posts").addEventListener("click", deletePost);
+
 function getPosts() {
   http
     .get("http://localhost:3000/posts")
     .then((data) => ui.showPosts(data))
     .catch((err) => console.log(err));
 }
+
+function submitPost() {
+  const title = document.querySelector("#title").value;
+  const body = document.querySelector("#body").value;
+
+  const data = {
+    title,
+    body,
+  };
+
+  // create post
+  http
+    .post("http://localhost:3000/posts", data)
+    .then((data) => {
+      ui.showAlert("Post Added", "alert alert-success");
+      ui.clearFields();
+      getPosts();
+    })
+    .catch((err) => console.log(err));
+}
+
+ 
